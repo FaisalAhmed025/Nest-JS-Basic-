d05c8cbe82369ec301d51ab2820e5c5db18e6a0a
Md Faisal Ahmed <md.faisal3@northsouth.edu>
2023-01-14 17:43:06 +0600

    node js basis structure

---
+++ src/users/dtos/createUser.dto.ts
@@ -0,0 +1,13 @@
+
+import { IsNotEmpty,IsEmail} from 'class-validator';
+
+
+export class  createUserDto{
+    @IsNotEmpty()
+    username: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+    
+}
